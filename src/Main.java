7e63d92aa47cdac88b959ff8188e43c27e22e520
alzahara <shahdaljabri33@gmail.com>
2026-04-07 15:37:22 +0400

    Task 18: lessBy10

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private Task17 Task17;
 
     public static void main(String[] args) {
-     //   Main program = new Main();
+        //   Main program = new Main();
 //String[] testCases = {"fig", "dib", "fib", "abc"};
 
 //for (String test : testCases) {
@@ -12,10 +12,17 @@ public class Main {
 //System.out.println(test + " -> " + result);}
 
 
-        Task17 program = new Task17();
-        System.out.println("output " + program.inOrder(1, 2, 4, false));
-        System.out.println("output " + program.inOrder(1, 2, 1, false));
-        System.out.println("output " + program.inOrder(1, 1, 2, true));
+        //Task17 program = new Task17();
+        //  System.out.println("output " + program.inOrder(1, 2, 4, false));
+        // System.out.println("output " + program.inOrder(1, 2, 1, false));
+        //System.out.println("output " + program.inOrder(1, 1, 2, true));
+
+        Task18 task18 = new Task18();
+
+
+        System.out.println("Result 1: " + task18.lessBy10(1, 7, 11));
+        System.out.println("Result 2: " + task18.lessBy10(1, 7, 10));
+        System.out.println("Result 3: " + task18.lessBy10(11, 1, 7));
     }
 
 
@@ -31,4 +38,5 @@ public class Main {
         }
     }
 }
+
 
